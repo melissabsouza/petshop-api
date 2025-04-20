3495173e25bcaed9f71e4344f8413508e8b54f8d
Melissa <melissabsouzaa@gmail.com>
2025-04-20 18:42:06 -0300

    feat: TutorController

--- src/main/java/fiap/tds/petshop/controller/TutorController.java
+++ src/main/java/fiap/tds/petshop/controller/TutorController.java
@@ -1,9 +1,63 @@
 package fiap.tds.petshop.controller;
 
+import fiap.tds.petshop.dto.TutorDTO;
+import fiap.tds.petshop.service.TutorService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RestController("/tutores")
+@AllArgsConstructor
+@Log
+@RequestMapping("/tutores")
 public class TutorController {
+
+    private TutorService tutorService;
+
+    @GetMapping
+    public String listarTutores(Model model) {
+        List<TutorDTO> lista = tutorService.getAllTutors();
+        model.addAttribute("tutores", lista);
+        return "tutores/lista";
+    }
+
+    @GetMapping("/novo")
+    public String novoTutor(Model model) {
+        model.addAttribute("tutor", new TutorDTO());
+        return "tutores/novo";
+    }
+
+    @PostMapping
+    public String salvarTutor(@Valid @ModelAttribute("tutor")
+                                  TutorDTO tutor,
+                              BindingResult bindingResults,
+                              Model model) {
+        if (bindingResults.hasErrors()){
+            bindingResults.getAllErrors().forEach(e-> log.info(e.toString()));
+            model.addAttribute("tutor", tutor);
+            return "tutores/formulario";
+        }
+        tutorService.saveTutor(tutor);
+        return "redirect:/tutores";
+    }
+
+    @GetMapping("/editar/{cpf}")
+    public String editarTutor(@PathVariable String cpf, Model model){
+          model.addAttribute("tutor", tutorService.findById(cpf));
+          return "tutores/formulario";
+    }
+
+    @GetMapping("/deletar/{cpf}")
+    public String excluirTutor(@PathVariable String cpf, Model model){
+        tutorService.deleteById(cpf);
+        return "redirect:/tutores";
+    }
+
+
 }
