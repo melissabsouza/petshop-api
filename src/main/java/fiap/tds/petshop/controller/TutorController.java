b2c1ffafef8353a885e7da8582044c48d8d2e690
Melissa <melissabsouzaa@gmail.com>
2025-04-20 18:29:12 -0300

    feat: tutorService

---
+++ src/main/java/fiap/tds/petshop/controller/TutorController.java
@@ -0,0 +1,9 @@
+package fiap.tds.petshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController("/tutores")
+public class TutorController {
+}
--- src/main/java/fiap/tds/petshop/service/TutorService.java
+++ src/main/java/fiap/tds/petshop/service/TutorService.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,22 +21,36 @@ public class TutorService {
     public TutorDTO saveTutor(TutorDTO tutorDTO) {
         Tutor tutor = toEntity(tutorDTO);
 
-//        if(tutorDTO .getCpf()==null){
-//            tutor = tutorRepository.save(tutor);
-//        } else{
-//            tutor.byId = this.findByCPF(tutorDTO.getCpf());
-//
-//
-       }
+        if(tutorDTO .getCpf()==null){
+            tutor = tutorRepository.save(tutor);
+        } else {
+            TutorDTO byId = this.findById(tutorDTO.getCpf());
+            byId.setEmail(tutorDTO.getEmail());
+            byId.setNome(tutorDTO.getNome());
+            byId.setTelefone(tutor.getTelefone());
 
-        public TutorDTO findById(String cpf) {
-            Optional<Tutor> byId = tutorRepository.findById(cpf);
-            if(byId.isPresent()) {
-                return toDto(byId.get());
-            }
-            throw new RuntimeException("cpf nao encontrado");
+            tutor = tutorRepository.save(toEntity(byId));
         }
+        return toDto(tutor);
 
+    }
+
+    public List<TutorDTO> getAllTutors() {
+        List<Tutor> tutors = tutorRepository.findAll();
+        List<TutorDTO> tutorDTOs = tutors.stream().map(TutorService::toDto).toList();
+        return tutorDTOs;
+    }
+
+    public void deleteById(String cpf) {
+        tutorRepository.deleteById(cpf);
+    }
+
+    public TutorDTO findById(String cpf) {
+        Optional<Tutor> byId = tutorRepository.findById(cpf);
+        if(byId.isPresent()) {
+            return toDto(byId.get());
+        }
+        throw new RuntimeException("cpf nao encontrado");
     }
 
 
@@ -50,7 +66,7 @@ public class TutorService {
 
     private static TutorDTO toDto(Tutor tutor) {
         TutorDTO tutorDTO = new TutorDTO();
-        tutorDTO.setCpf(tutor.getCpf()); // Certifique-se que está usando getCpf()
+        tutorDTO.setCpf(tutor.getCpf());
         tutorDTO.setNome(tutor.getNome());
         tutorDTO.setEmail(tutor.getEmail());
         tutorDTO.setTelefone(tutor.getTelefone());
